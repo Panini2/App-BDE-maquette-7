2c4c8727aed2924cef77f98d82e19ddd00d65197
Bapisto <baptiste.rachez@epitech.eu>
2024-11-15 15:47:34 +0100

    [ADD] app test

--- burger-menu.js
+++ burger-menu.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.querySelector('.start-button');
     const offscreenMenus = document.querySelectorAll('.offscreen-menu'); // Select all elements with 'offscreen-burger'
     const logoutButton = document.querySelector('.logout');
+    const testButton = document.querySelector('.test');
 
     startButton.addEventListener('click', () => {
         // Loop through all elements with the 'offscreen-burger' class and toggle it
@@ -12,4 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
     logoutButton.addEventListener('click', function () {
         window.location.href = 'index.html';
     });
+});
+document.querySelector('.app.test').addEventListener('click', () => {
+    const windowAppTest = document.querySelector('.window-app-test');
+
+    // Toggle the `hidden` class to make it disappear
+    windowAppTest.classList.add('active');
+});
+document.querySelector('.Close').addEventListener('click', () => {
+    const windowAppTest = document.querySelector('.window-app-test');
+
+    // Toggle the `hidden` class to make it disappear
+    windowAppTest.classList.remove('active');
 });
